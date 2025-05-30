5938e6239d8cad759c0eef274e73d666e6fd599a
NLSS | A Bilgehan SERCE <bilgehanserce@nlssmuhendislik.com.tr>
2025-05-30 20:32:32 +0300

    [Success] QOL Changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,9 @@ set -e
 source "$HOME/.venv/bin/activate"
 
 # Check the Tensorflow installation & write it to a log file.
-python3 "$HOME/dev/run.py"
+if [ -f "$HOME/.dev/run.py" ]; then
+    python3 "$HOME/.dev/run.py"
+fi
 # Remove the script once it finishes executing.
 # rm -rf $HOME/dev/run.py
 
